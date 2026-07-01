7dc2d4565b0edef490a35c8fa166e28cef69e586
Yen-Chieh Huang <lashhw81@gmail.com>
2026-07-01 12:13:20 +0800

    add fig10.sh

---
+++ scripts/fig10.sh
@@ -0,0 +1,13 @@
+set -euo pipefail
+
+[[ "$OPTION" =~ ^(neither|BPonly|PConly)-(shufflenet|mobilenet|inception)-vm(96|128|256)$ ]] || { echo "Error: Invalid OPTION"; exit 1; }
+
+EXGRULE="${BASH_REMATCH[1]}"
+MODEL="${BASH_REMATCH[2]}"
+VM="${BASH_REMATCH[3]}"
+
+echo "Executing exgrule: $EXGRULE, model: $MODEL, VM: $VM..."
+
+cd HEtest
+bash "run_${MODEL}_exgrule.sh" "$EXGRULE" "$VM"
+python3.9 collect_allseed_report.py --exgrule "$EXGRULE" --model "$MODEL" --vm_setting "$VM"
